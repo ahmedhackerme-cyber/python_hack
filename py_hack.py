90312263f56957f167b39feb86c1347b4102b9e4
ahmedhackerme-cyber <ahmedhackerme@gmail.com>
2025-10-17 19:01:25 +0200

    Add hacker web interface with URL functionality
    Implement a basic hacker web interface with options to open a web page or check the status of a URL.

---
+++ py_hack.py
@@ -0,0 +1,58 @@
+import colorama as c
+import os
+import subprocess as s
+import time
+import webbrowser as wb
+
+time.sleep(1)
+s.run("clear.bat")
+c.init()
+
+f = c.Fore
+print(f.YELLOW+"["+f.RED+"1"+f.YELLOW+"]"+f.WHITE+" hacker web")
+print(" ")
+print(f.YELLOW+"["+f.RED+"0"+f.YELLOW+"]"+f.RED+" exit")
+
+print("")
+i = input(" > ")
+
+if i == "0":
+    exit()
+elif i == "1":
+    time.sleep(1)
+    s.run("clear.bat")
+    c.init()
+
+    f = c.Fore
+    print(f.YELLOW+"["+f.RED+"1"+f.YELLOW+"]"+f.WHITE+" open web")
+    print(" ")
+    print(f.YELLOW+"["+f.RED+"2"+f.YELLOW+"]"+f.WHITE+" search page web")
+    print(" ")
+    print(f.YELLOW+"["+f.RED+"0"+f.YELLOW+"]"+f.RED+" exit")
+    print(" ")
+    i = input(" > hacker web > ")
+    if i == "1":
+        s.run("clear.bat")
+        c.init()
+        while True:
+            print(f.RED)
+            w = input(" > hacker web > open web > web is : ")
+            if w == "exit":
+                print("exit")
+                print(f.WHITE+" ------------------------- "+f.RED+"Stop"+f.WHITE+" -------------------------------")
+            wb.open("https://"+w+"/")
+            print(" url = "+f.GREEN+"https://"+w+"/"+f.WHITE+"------------------------------")
+    elif i == "2":
+        import requests
+        
+        def e(url, timeout=5):
+            try:
+                r = requests.head(url, allow_redirects=True, timeout=timeout)
+                print(f.RED+r.status_code < 400)
+            except requests.RequestException:
+                print(f.RED+"False")
+        s.run("clear.bat")
+        while True:
+            i = input(" > hacker web > search page web > page is : ")
+            print(" ")
+            e(i)
